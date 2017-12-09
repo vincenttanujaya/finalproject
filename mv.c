df3cf2d686b7a8cf51e357ece2b5e7fbc0728cf4
vincenttanujaya <vincent.tanujaya@gmail.com>
2017-12-09 08:02:24 +0700

    batasan

--- mv.c
+++ mv.c
@@ -3,6 +3,10 @@
 #include "user.h"
 
 int main (int argc, char *argv[]){
+  if (argc != 3){
+    printf(2, "Usage: mv oldname newname\n");
+    exit();
+  }
   if ((link(argv[1], argv[2]) < 0) || (unlink(argv[1]) < 0))
     printf(2, "mv %s to %s failed\n", argv[1], argv[2]);
 
