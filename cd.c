15cba9ca987ac378d062b9d6c58f6f6a0b030854
vincenttanujaya <vincent.tanujaya@gmail.com>
2017-12-09 07:55:07 +0700

    lagi

--- cd.c
+++ cd.c
@@ -4,9 +4,8 @@
 #include "fcntl.h"
 
 int main(int argc, char *argv[]){
-
-  if(argc!=2) printf("Needs 2 argumenrts\n");
-  
+  if(argc==1) chdir("/");
+  else if(argc!=2) printf(1,"Needs 2 argumenrts\n");
   else if(chdir(argv[1])<0)
   {
     printf(1,"Failed to change directory\n");
